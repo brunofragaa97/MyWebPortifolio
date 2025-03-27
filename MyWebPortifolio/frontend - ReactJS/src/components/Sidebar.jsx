355bc9729d308b4c2daeff103ca83b37ccba2939
Bruno de Fraga <brunofragaa97@gmail.com>
2025-03-27 11:40:43 -0300

    Adicionado qr code no sidebar / adicioado icones github e linkedin sidebar

--- MyWebPortifolio/frontend - ReactJS/src/components/Sidebar.jsx
+++ MyWebPortifolio/frontend - ReactJS/src/components/Sidebar.jsx
@@ -13,6 +13,13 @@ const Sidebar = () => {
         <p>Telefone: (51) 98904-3802</p>
       </div>
       <button className="btn1">Baixar Curriculum</button>
+      <div className="img-qr-code">
+        <img src="https://imgur.com/r6LylG9.png" alt="QR Code" referrerPolicy="no-referrer" />
+      </div>
+      <div className="redes-icons">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/LinkedIn_icon.svg/2048px-LinkedIn_icon.svg.png" />
+        <img src="https://cdn.worldvectorlogo.com/logos/github-icon-2.svg" />
+      </div>
     </aside>
   );
 };
